80db1ea4b0976a11b51215c32d3a425e192012fd
EC2 Default User <ec2-user@ip-172-31-73-76.ec2.internal>
2019-10-31 20:36:36 +0000

    add react app

---
+++ lndb-site/precache-manifest.ecf0296dc083aeed149ce79a93590ecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e974de09c0c0583d009a7641738599f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8988ef28ead2c936a66",
+    "url": "/static/css/main.16d49e58.chunk.css"
+  },
+  {
+    "revision": "d911293004ecb59ee172",
+    "url": "/static/js/2.0d812863.chunk.js"
+  },
+  {
+    "revision": "f8988ef28ead2c936a66",
+    "url": "/static/js/main.6d711e40.chunk.js"
+  },
+  {
+    "revision": "12d9d97ff08258acdd83",
+    "url": "/static/js/runtime~main.16eb02e5.js"
+  }
+]);
