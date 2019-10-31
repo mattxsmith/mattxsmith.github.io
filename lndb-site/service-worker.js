cc3e129f110f7e814bf29e4c0aead5ba2b81a949
EC2 Default User <ec2-user@ip-172-31-73-76.ec2.internal>
2019-10-31 20:51:32 +0000

    foo

--- lndb-site/service-worker.js
+++ lndb-site/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecf0296dc083aeed149ce79a93590ecc.js"
+  "/lndb-site/precache-manifest.b846bd33c20881c8d69f7764f399cc0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lndb-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
